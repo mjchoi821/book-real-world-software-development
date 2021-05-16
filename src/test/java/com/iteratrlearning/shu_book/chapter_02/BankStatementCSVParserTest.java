5bae859c3c08c713be5d664c129596e21dd9a3c4
mjchoi <mjchoi821@gmail.com>
2021-05-17 04:29:45 +0900

    test: CSV 파서의 parseFrom 메서드 유닛 테스트 추가

--- src/test/java/com/iteratrlearning/shu_book/chapter_02/BankStatementCSVParserTest.java
+++ src/test/java/com/iteratrlearning/shu_book/chapter_02/BankStatementCSVParserTest.java
@@ -2,16 +2,30 @@ package com.iteratrlearning.shu_book.chapter_02;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * 예제 2-14 CSV 파서의 유닛 테스트가 실패하는 경우
+ * 예제 2-15 assertion 구문 사용하기
  */
 public class BankStatementCSVParserTest {
     private final BankStatementParser statementParser = new BankStatementCSVParser();
 
     @Test
-    public void shouldParseOneCorrectLine() throws Exception {
-        fail("Not yet implemented");
+    public void shouldParseOneCorrectLine() {
+        // Given
+        final String line = "30-01-2017,-50,Tesco";
+        final BankTransaction expected = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
+        final double tolerance = 0.0d;
+
+        // When
+        final BankTransaction result = statementParser.parseFrom(line);
+
+        // Then
+        assertEquals(expected.getDate(), result.getDate());
+        assertEquals(expected.getAmount(), result.getAmount(), tolerance);
+        assertEquals(expected.getDescription(), result.getDescription());
     }
 }
