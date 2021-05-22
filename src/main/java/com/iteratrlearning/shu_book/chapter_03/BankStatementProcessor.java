d246486465e91168ebeb39cf4f60652b4eab4509
mjchoi <mjchoi821@gmail.com>
2021-05-23 01:39:22 +0900

    feat: 거래 내역 찾기 기능 구현(특정 금액 이상 조회, 특정 월 조회)

--- src/main/java/com/iteratrlearning/shu_book/chapter_03/BankStatementProcessor.java
+++ src/main/java/com/iteratrlearning/shu_book/chapter_03/BankStatementProcessor.java
@@ -1,10 +1,13 @@
 package com.iteratrlearning.shu_book.chapter_03;
 
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 예제 2-7 BankStatementProcessor 클래스의 계산 연산 그룹화
+ * 예제 3-1 특정 금액 이상의 은행 거래 내역 찾기
+ * 예제 3-2 특정 월의 입출금 내역 찾기
  */
 public class BankStatementProcessor {
     private final List<BankTransaction> bankTransactions;
@@ -39,5 +42,25 @@ public class BankStatementProcessor {
             }
         }
         return total;
+    }
+
+    public List<BankTransaction> findTransactionsGreaterThanEqual(final int amount) {
+        final List<BankTransaction> result = new ArrayList<>();
+        for (final BankTransaction bankTransaction: bankTransactions) {
+            if (bankTransaction.getAmount() >= amount) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
+
+    public List<BankTransaction> findTransactionInMonth(final Month month) {
+        final List<BankTransaction> result = new ArrayList<>();
+        for (final BankTransaction bankTransaction: bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
     }
 }
