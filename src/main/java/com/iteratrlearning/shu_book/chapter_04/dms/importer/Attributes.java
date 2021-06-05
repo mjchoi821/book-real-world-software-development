e84ff33c968fb98ae7914dd23011caed78221136
mjchoi <mjchoi821@gmail.com>
2021-06-06 05:07:46 +0900

    feat: 이미지 임포터 구현

---
+++ src/main/java/com/iteratrlearning/shu_book/chapter_04/dms/importer/Attributes.java
@@ -0,0 +1,8 @@
+package com.iteratrlearning.shu_book.chapter_04.dms.importer;
+
+public class Attributes {
+    public static final String PATH = "path";
+    public static final String WIDTH = "width";
+    public static final String HEIGHT = "height";
+    public static final String TYPE = "type";
+}
