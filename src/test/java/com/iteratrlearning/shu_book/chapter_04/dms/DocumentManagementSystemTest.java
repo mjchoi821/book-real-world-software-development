26c4b4f11987db99e2beaae5957a0b379aeb86ad
mjchoi <mjchoi821@gmail.com>
2021-06-20 00:52:17 +0900

    test: DocumentManagementSystemTest 예외 테스트 추가 및 상수, 테스트 코드 정리

--- src/test/java/com/iteratrlearning/shu_book/chapter_04/dms/DocumentManagementSystemTest.java
+++ src/test/java/com/iteratrlearning/shu_book/chapter_04/dms/DocumentManagementSystemTest.java
@@ -1,42 +1,33 @@
 package com.iteratrlearning.shu_book.chapter_04.dms;
 
 import com.iteratrlearning.shu_book.chapter_04.dms.exception.UnknownFileTypeException;
+import com.iteratrlearning.shu_book.chapter_04.dms.importer.Attributes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.List;
 
 import static com.iteratrlearning.shu_book.chapter_04.dms.importer.Attributes.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DocumentManagementSystemTest {
-    private static final String RESOURCES = "target/test-classes/";
+    private static final String RESOURCES = "src" + File.separator + "test" + File.separator + "resources" + File.separator;
+    private static final String LETTER = RESOURCES + "patient.letter";
+    private static final String REPORT = RESOURCES + "patient.report";
+    private static final String XRAY = RESOURCES + "xray.jpg";
+    private static final String INVOICE = RESOURCES + "patient.invoice";
 
-    @Test
-    void shouldImportLetterAttributes() throws Exception {
-        // given
-        final String fileName = "patient.letter";
-        final Path path = Paths.get(RESOURCES + fileName);
-        DocumentManagementSystem system = new DocumentManagementSystem();
+    final DocumentManagementSystem system = new DocumentManagementSystem();
 
-        // when
-        system.importFile(path.toAbsolutePath().toString());
-        final Document document = system.contents().get(0);
-
-        // then
-        assertAttributeEquals(document, PATIENT, "Joe Bloggs");
-        assertAttributeEquals(document, ADDRESS,
-                "123 Fake Street\n" +
-                "Westminster\n" +
-                "London\n" +
-                "United Kingdom");
-        assertAttributeEquals(document, BODY,
-                "We are writing to you to confirm the re-scheduling of your appointment\n" +
-                "with Dr. Avaj from 29th December 2016 to 5th January 2017.");
-        assertEquals("LETTER", document.getAttribute(TYPE));
+    private Document onlyDocument() {
+        List<Document> documents = system.contents();
+        assertThat(documents, hasSize(1));
+        return documents.get(0);
     }
 
     private void assertAttributeEquals(Document document, String attributeName, String expectedValue) {
@@ -44,14 +35,64 @@ class DocumentManagementSystemTest {
     }
 
     @Test
+    public void shouldImportFile() throws Exception {
+        // given
+        system.importFile(LETTER);
+
+        // when
+        final Document document = onlyDocument();
+
+        // then
+        assertAttributeEquals(document, Attributes.PATH, LETTER);
+    }
+
+    @Test
+    void shouldImportLetterAttributes() throws Exception {
+        // given
+        system.importFile(LETTER);
+
+        // when
+        final Document document = onlyDocument();
+
+        // then
+        assertAttributeEquals(document, PATIENT, "Joe Bloggs");
+        assertAttributeEquals(document, ADDRESS,
+                "123 Fake Street\n" +
+                        "Westminster\n" +
+                        "London\n" +
+                        "United Kingdom");
+        assertAttributeEquals(document, BODY,
+                "We are writing to you to confirm the re-scheduling of your appointment\n" +
+                        "with Dr. Avaj from 29th December 2016 to 5th January 2017.");
+        assertEquals("LETTER", document.getAttribute(TYPE));
+    }
+
+    @Test
+    void shouldNotImportMissingFile() {
+        assertThrows(FileNotFoundException.class, () ->
+                system.importFile("gobbledygook.txt"));
+    }
+
+    @Test
+    void shouldNotImportUnknownFile() {
+        assertThrows(UnknownFileTypeException.class, () ->
+                system.importFile(RESOURCES + "unknown.txt"));
+    }
+
+    /**
+     * =================
+     *  추가로 만든 테스트들
+     * =================
+     */
+
+    @Test
     @DisplayName("importFile 동작 시 path 가 유효하지 않은 값일 경우 예외 발생")
     void importFile_whenDerivedException_invalidPathString() {
         // given
-        DocumentManagementSystem dms = new DocumentManagementSystem();
         String expectedMessage = "scam_path";
 
         // when
-        Exception exception = assertThrows(FileNotFoundException.class, () -> dms.importFile("scam_path"));
+        Exception exception = assertThrows(FileNotFoundException.class, () -> system.importFile("scam_path"));
         String actualMessage = exception.getMessage();
 
         // then
@@ -62,12 +103,11 @@ class DocumentManagementSystemTest {
     @DisplayName("importFile 동작 시 처리할 수 없는 확장자일 경우 예외 발생 테스트")
     void importFile_whenDerivedException() {
         // given
-        DocumentManagementSystem dms = new DocumentManagementSystem();
-        String testImportPath = getClass().getClassLoader().getResource("unknown.txt").getFile();
-        String expectedMessage = "For file: " + testImportPath;
+        final String UNKNOWN = RESOURCES + "unknown.txt";
+        String expectedMessage = "For file: " + UNKNOWN;
 
         // when
-        Exception exception = assertThrows(UnknownFileTypeException.class, () -> dms.importFile(testImportPath));
+        Exception exception = assertThrows(UnknownFileTypeException.class, () -> system.importFile(UNKNOWN));
         String actualMessage = exception.getMessage();
 
         // then
