87c7ff17a536fbd2e9572b05f6eb28c2665c533a
mjchoi <mjchoi821@gmail.com>
2021-06-20 01:26:55 +0900

    feat: 테스트를 통과하는 기능 구현

--- src/main/java/com/iteratrlearning/shu_book/chapter_05/BusinessRuleEngine.java
+++ src/main/java/com/iteratrlearning/shu_book/chapter_05/BusinessRuleEngine.java
@@ -1,13 +1,21 @@
 package com.iteratrlearning.shu_book.chapter_05;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BusinessRuleEngine {
+    private final List<Action> actions;
+
+    public BusinessRuleEngine() {
+        this.actions = new ArrayList<>();
+    }
 
     public void addAction(Action action) {
-        throw new UnsupportedOperationException();
+        this.actions.add(action);
     }
 
     public int count() {
-        throw new UnsupportedOperationException();
+        return this.actions.size();
     }
 
     public void run() {
